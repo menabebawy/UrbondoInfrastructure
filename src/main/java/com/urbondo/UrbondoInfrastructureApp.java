ea1c30d44e56e291566dfd37e750a9194abb89fe
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-06-20 16:32:26 +0200

    add system variables for cdk region and account number

---
+++ src/main/java/com/urbondo/UrbondoInfrastructureApp.java
@@ -0,0 +1,21 @@
+package com.urbondo;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+public class UrbondoInfrastructureApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new UrbondoInfrastructureStack(app, "UrbondoInfrastructureStack", StackProps.builder()
+                .env(Environment.builder()
+                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                        .region(System.getenv("CDK_DEFAULT_REGION"))
+                        .build())
+
+                .build());
+
+        app.synth();
+    }
+}
