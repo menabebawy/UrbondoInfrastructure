dcc19aa6012104717c5b2bef0331d52e4fb3fb7e
Mena Bebawy <eng.mena.lorans@gmail.com>
2023-10-13 20:32:57 +0200

    update api endpoints

--- infrastructure/src/main/java/com/urbondo/UrbondoStack.java
+++ infrastructure/src/main/java/com/urbondo/UrbondoStack.java
@@ -2,7 +2,7 @@ package com.urbondo;
 
 import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.*;
-import software.amazon.awscdk.services.apigateway.IResource;
+import software.amazon.awscdk.services.apigateway.Resource;
 import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.dynamodb.*;
 import software.amazon.awscdk.services.lambda.Code;
@@ -12,12 +12,15 @@ import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 
 import static software.amazon.awscdk.services.apigateway.MethodLoggingLevel.INFO;
-import static software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod.ANY;
+import static software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod.GET;
 import static software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod.POST;
 import static software.amazon.awscdk.services.logs.RetentionDays.ONE_WEEK;
 
 public class UrbondoStack extends Stack {
     private static final Number READ_CAPACITY = 1;
+    private static final String USER = "user";
+    private static final String CATEGORY = "category";
+    private static final String ANNOUNCEMENT = "announcement";
 
     public UrbondoStack(@Nullable Construct scope, @Nullable String id, @Nullable StackProps props) {
         super(scope, id, props);
@@ -35,44 +38,67 @@ public class UrbondoStack extends Stack {
 
         RestApi restApi = createRestApi(lambdaFunction);
 
-        new CfnOutput(this, "HttApi", CfnOutputProps.builder().description("Url for Http Api").value(restApi.getUrl()).build());
+        new CfnOutput(this,
+                      "HttApi",
+                      CfnOutputProps.builder().description("Url for Http Api").value(restApi.getUrl()).build());
     }
 
     private Function createLambdaFunction() {
-        return new Function(this, "PrintRequest", FunctionProps.builder().runtime(Runtime.JAVA_17).code(Code.fromAsset("../lambda/build/distributions/lambda-0.1.zip")).handler("com.urbondo.PrintApiGatewayRequest").memorySize(1024).timeout(Duration.seconds(10)).logRetention(ONE_WEEK).build());
+        return new Function(this,
+                            "PrintRequest",
+                            FunctionProps.builder()
+                                    .runtime(Runtime.JAVA_17)
+                                    .code(Code.fromAsset("../lambda/build/distributions/lambda-0.1.zip"))
+                                    .handler("com.urbondo.PrintApiGatewayRequest")
+                                    .memorySize(1024)
+                                    .timeout(Duration.seconds(10))
+                                    .logRetention(ONE_WEEK)
+                                    .build());
     }
 
     private RestApi createRestApi(Function lambdaFunction) {
-        RestApiProps restApiProps = RestApiProps.builder().restApiName("Urbondo-REST-Api").deployOptions(StageOptions.builder().loggingLevel(INFO).build()).build();
+        RestApiProps restApiProps = RestApiProps.builder()
+                .restApiName("Urbondo-REST-Api")
+                .deployOptions(StageOptions.builder().loggingLevel(INFO).build())
+                .build();
 
         RestApi restApi = new RestApi(this, "Urbondo-REST", restApiProps);
 
-        ResourceOptions resourceOptions = ResourceOptions.builder().defaultIntegration(LambdaIntegration.Builder.create(lambdaFunction).build()).build();
+        ResourceOptions resourceOptions = ResourceOptions.builder()
+                .defaultIntegration(LambdaIntegration.Builder.create(lambdaFunction).build())
+                .build();
 
-        addResource(restApi.getRoot(), "user", resourceOptions);
-        addResource(restApi.getRoot(), "announcement", resourceOptions);
-        addResource(restApi.getRoot(), "category", resourceOptions);
+        Resource userResource = restApi.getRoot()
+                .addResource(USER, resourceOptions);
+        userResource.addResource("signup").addMethod(POST.name());
+        userResource.addResource("login").addMethod(POST.name());
+        userResource.addResource("{id}").addMethod(GET.name());
+
+        Resource categoryResource = restApi.getRoot()
+                .addResource(CATEGORY, resourceOptions);
+        categoryResource.addMethod(POST.name());
+        categoryResource.addResource("{id}").addMethod(GET.name());
+
+        Resource announcementResource = restApi.getRoot()
+                .addResource(ANNOUNCEMENT, resourceOptions);
+        announcementResource.addMethod(POST.name());
+        announcementResource.addResource("{id}").addMethod(GET.name());
 
         return restApi;
     }
 
-
-    private static void addResource(IResource rootResource, String path, ResourceOptions resourceOptions) {
-        rootResource.addResource(path, resourceOptions).addMethod(POST.name()).getResource().addResource("{id}").addMethod(ANY.name());
-    }
-
     private Table createUserTable() {
-        Table userTable = createTableBySuffix("user");
+        Table userTable = createTableBySuffix(USER);
         userTable.addGlobalSecondaryIndex(globalSecondaryIndexProps("email"));
         return userTable;
     }
 
     private Table createAnnouncementTable() {
-        return createTableBySuffix("announcement");
+        return createTableBySuffix(ANNOUNCEMENT);
     }
 
     private Table createCategoryTable() {
-        Table categoryTable = createTableBySuffix("category");
+        Table categoryTable = createTableBySuffix(CATEGORY);
         categoryTable.addGlobalSecondaryIndex(globalSecondaryIndexProps("title"));
         return categoryTable;
     }
@@ -81,12 +107,23 @@ public class UrbondoStack extends Stack {
         String prefix = "urbondo";
         String idAttributeName = "id";
 
-        TableProps tableProps = TableProps.builder().partitionKey(Attribute.builder().name(idAttributeName).type(AttributeType.STRING).build()).readCapacity(READ_CAPACITY).writeCapacity(READ_CAPACITY).removalPolicy(RemovalPolicy.DESTROY).tableName(prefix + "-" + name).build();
+        TableProps tableProps = TableProps.builder()
+                .partitionKey(Attribute.builder().name(idAttributeName).type(AttributeType.STRING).build())
+                .readCapacity(READ_CAPACITY)
+                .writeCapacity(READ_CAPACITY)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .tableName(prefix + "-" + name)
+                .build();
 
         return new Table(this, name, tableProps);
     }
 
     private static GlobalSecondaryIndexProps globalSecondaryIndexProps(String attributeName) {
-        return GlobalSecondaryIndexProps.builder().indexName(attributeName + "-index").partitionKey(Attribute.builder().name(attributeName).type(AttributeType.STRING).build()).readCapacity(READ_CAPACITY).writeCapacity(READ_CAPACITY).build();
+        return GlobalSecondaryIndexProps.builder()
+                .indexName(attributeName + "-index")
+                .partitionKey(Attribute.builder().name(attributeName).type(AttributeType.STRING).build())
+                .readCapacity(READ_CAPACITY)
+                .writeCapacity(READ_CAPACITY)
+                .build();
     }
 }
